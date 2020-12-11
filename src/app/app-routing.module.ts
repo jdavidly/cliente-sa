1020794f8d223d2f65a7553e6a1855b519641522
huriel2g <hurieluv2ged93@gmail.com>
2020-12-11 06:04:24 -0600

    add productos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },  {
+    path: 'add-products',
+    loadChildren: () => import('./add-products/add-products.module').then( m => m.AddProductsPageModule)
   },
+
 ];
 
 @NgModule({
